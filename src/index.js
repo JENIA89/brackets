27110994f74ecaf383bf8d80f67d70bf7990fbf9
Evgene Popolamov <popolamov908@gmail.com>
2020-10-06 20:28:47 +0300

    feat: add solve brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const confArr = bracketsConfig.map((item) => item.join(""));
+
+    for (let i = 0; i < confArr.length; ) {
+        if (str.includes(confArr[i])) {
+            str = str.replace(confArr[i], "");
+            i = 0;
+            continue;
+        }
+        i++;
+    }
+
+    return str.length === 0 ? true : false;
+};
